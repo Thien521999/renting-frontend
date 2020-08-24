24aa0d03251efd283c97327f7afab0e9acb8153d
hung.nguyendang01 <hung.nguyendang@pycogroup.com>
2020-08-24 17:06:36 +0700

    will definitely run on heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const port = process.env.PORT || 3000;
-
+console.log(`port: ${port}`);
 const app = express();
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
@@ -13,4 +13,4 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, '0.0.0.0', () => console.log(`Listening on ${port}`));
+app.listen(port, '0.0.0.0');
